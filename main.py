554ac2e235d29b8726d1687867ef0f082985e603
student <ssdsadsadad@gmail.com>
2026-05-22 05:50:11 +0300

    domashnia

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello")
+from colorama import init, Fore, Back, Style
+init()
+
+a = input(Fore.YELLOW + "a:")
+
+try:
+    print(Fore.BLUE + f'{int(a)}')
+except ValueError as ve:
+    print(Fore.RED + "Неможливо конвертувати в число")
